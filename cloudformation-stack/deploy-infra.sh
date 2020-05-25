f92b11775e2e4f858f4048554044b124f30152f3
Anubhav Y <contact+code@anubhav.codes>
2020-05-25 14:54:47 +0200

    🎒 Add codebuild / codedeploy spec

--- cloudformation-stack/deploy-infra.sh
+++ cloudformation-stack/deploy-infra.sh
@@ -3,8 +3,23 @@ STACK_NAME=awsbootstrap
 REGION=eu-central-1
 EC2_INSTANCE_TYPE=t2.micro
 
+AWS_ACCOUNT_ID=`docker run --rm -it -v ~/.aws:/root/.aws -v $(pwd):/aws amazon/aws-cli sts get-caller-identity --profile awsbootstrap \
+  --query "Account" --output text`
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID" 
+
+# Deploy the setup.yml template. S3 buckets
+echo -e "\n\n==========Deploying main.yml=============="
+docker run --rm -it -v ~/.aws:/root/.aws -v $(pwd):/aws amazon/aws-cli cloudformation deploy \
+  --region $REGION \
+  --stack-name $STACK_NAME-setup \
+  --template-file setup.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
+
 # Deploy the cloudformation template
-echo -e ==========Deploying main.yml==============
+echo -e "\n\n==========Deploying main.yml=============="
 docker run --rm -it -v ~/.aws:/root/.aws -v $(pwd):/aws amazon/aws-cli cloudformation deploy \
   --region $REGION \
   --stack-name $STACK_NAME \
